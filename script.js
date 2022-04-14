71e7cb369d67e5fea0aebf5f88376a77725211da
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-14 08:52:18 +0530

    Add a function which acts based on a specific operator

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function mul(a, b) {
 function div(a, b) {
     return Math.round(a / b);
 }
+
+function operate(action, a, b) {
+    if (action === 'a') return add(a, b);
+    else if (action === 's') return sub(a, b);
+    else if (action === 'm') return mul(a, b);
+    else if (action === 'd') return div(a, b);
+}
